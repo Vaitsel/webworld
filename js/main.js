2e42b4ef7950d18d9b7eaa8e35ba61f6467d9388
Vaitsel <0z.vaitsel@gmail.com>
2021-01-28 02:37:39 +0300

    with footer

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('.intro .swiper-container', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
 
       pagination: {
-        el: '.swiper-pagination',
+        el: '.intro .swiper-pagination',
         clickable: true,
       },
+
+});
+
+var mySwiper = new Swiper('.testimonials .swiper-container', {
+    pagination: {
+      el: '.testimonials .swiper-pagination',
+      clickable: true,
+    },
 
 });
