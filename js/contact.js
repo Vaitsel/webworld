db8e6807cb9eede6eccd3974bac51cc2f7b24498
Vaitsel <0z.vaitsel@gmail.com>
2021-02-27 15:50:57 +0300

    fixed  comments by Alexander

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,8 @@
 let btn_burger = document.querySelector('.intro-burger');
 let nav_burger = document.querySelector('.logo-nav');
-let title_burger = document.querySelector('h1');
 
 btn_burger.addEventListener('click', function(){
   this.classList.toggle('active');
   nav_burger.classList.toggle('show');
-  title_burger.classList.toggle('title-show');
 });
 
