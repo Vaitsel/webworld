9f7b3ab360ecd959fa0e53d20053d5205c17f0c8
Vaitsel <0z.vaitsel@gmail.com>
2021-03-01 01:18:34 +0300

    after gulp

--- js/contact.js
+++
@@ -1,8 +0,0 @@
-let btn_burger = document.querySelector('.intro-burger');
-let nav_burger = document.querySelector('.logo-nav');
-
-btn_burger.addEventListener('click', function(){
-  this.classList.toggle('active');
-  nav_burger.classList.toggle('show');
-});
-
